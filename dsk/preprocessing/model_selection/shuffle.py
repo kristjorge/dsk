251eec57388241f6c8be27f862e44a9dc6edaad9
Kristian Jørgensen <kristjorge@gmail.com>
2020-06-04 23:10:04 +0200

    Caught IndexError in shuffle.py

--- dsk/preprocessing/model_selection/shuffle.py
+++ dsk/preprocessing/model_selection/shuffle.py
@@ -4,7 +4,7 @@ import numpy as np
 def shuffle(*args, axis=0):
 
     # Assert that all arguments are of the same dimension in the specified axis (0 == row, 1 == column)
-    assert all(arg.shape[axis] == args[axis].shape[axis] for arg in args)
+    # assert all(arg.shape[axis] == args[axis].shape[axis] for arg in args)
 
     # Create an array of a permutation of the indices for 0 - N of the indices of the original arrays
     permutation = np.random.permutation(np.arange(args[0].shape[axis]))
@@ -13,9 +13,14 @@ def shuffle(*args, axis=0):
     for idx in permutation:
         for new_ind_idx, new_ind in enumerate(shuffled):
             if axis == 0:
-                new_ind.append(args[new_ind_idx][idx, :])
+                try:
+                    new_ind.append(args[new_ind_idx][idx, :])
+                except IndexError:
+                    new_ind.append(args[new_ind_idx][idx])
             elif axis == 1:
-                new_ind.append(args[new_ind_idx][:, idx])
-
+                try:
+                    new_ind.append(args[new_ind_idx][:, idx])
+                except IndexError:
+                    new_ind.append(args[new_ind_idx][idx])
 
     return [s for s in shuffled]
